b1fd3f250d408039383efca6e780d43024f35a5e
kieuvu <kieuvuvuvu@gmail.com>
2022-01-30 22:00:06 +0700

    config mongodb

--- src/ultis/db/index.js
+++ src/ultis/db/index.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 async function connect() {
   try {
-    await mongoose.connect(process.env.DB_URI);
+    // await mongoose.connect(process.env.DB_URI);
+    await mongoose.connect("mongodb+srv://kieuvu_vshop:kieuminhvu1@cluster0.wpjf5.mongodb.net/test?authSource=admin&replicaSet=atlas-1011fy-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true");
     console.log(`Connect to database successfully!!!`);
   } catch (error) {
     console.log(`Connect to database failure!!!`);
